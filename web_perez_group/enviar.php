71b7aaa38da64c680b3888cfa207a9636b309570
Daianatk <daiana.campos04@gmail.com>
2019-09-13 22:10:03 -0500

    email

---
+++ web_perez_group/enviar.php
@@ -0,0 +1,21 @@
+<?php
+
+$mail='daiana.campos04@gmail.com';
+
+
+$nombre = $_POST['nombre'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$thank="gracias.html";
+
+$message = "
+nombre:".$nombre."
+phone:".$phone."
+email:".$email."
+mensaje:".$mensaje."";
+
+if (mail($mail,"Formulario de Consulta", $message))
+Header ("Location:", $thank);
+?>
